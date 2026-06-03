ce4ce06ade21cdc8be75d1aed1703cce260f81bf
Shahid <109452231+raza-shahi@users.noreply.github.com>
2026-06-03 11:29:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded',()=>{
+	const output = document.createElement('p');
+	output.innerText = "DOM load success";
+	document.body.appendChild(output);
+})
